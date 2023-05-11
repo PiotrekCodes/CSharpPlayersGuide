c3eb0c9970bac76950f4d58b8d2d5498396162ab
Piotr Krawczyk <sztymperjan@gmail.com>
2023-05-11 19:00:38 +0100

    feat: added enums quest solution

---
+++ SimulasTestEnums/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Simula's Test");
+
+ChestState currentState = ChestState.locked;
+
+while (true)
+{
+    Console.WriteLine($"The chest is now {currentState}. What do you want to do? ");
+
+    string input = Console.ReadLine();
+
+    if(currentState == ChestState.locked && input == "unlock") currentState = ChestState.closed;
+    if(currentState == ChestState.closed && input == "open") currentState = ChestState.open;
+    if(currentState == ChestState.open &&  input == "close") currentState = ChestState.closed;
+    if (currentState == ChestState.closed && input == "lock") currentState= ChestState.locked;
+}
+
+
+
+enum ChestState
+{
+    open,
+    closed,
+    locked
+}
