7cbf259a01918fa0b96230e40570c1bf7d62df70
Piotr Krawczyk <sztymperjan@gmail.com>
2023-06-03 09:14:02 +0100

    feat: solved The Point exercise

---
+++ ThePoint/Program.cs
@@ -0,0 +1,18 @@
+﻿var point1 = new Point(2, 3);
+var point2 = new Point(-4, 0);
+
+Console.WriteLine($"({point1.X} {point1.Y})");
+Console.WriteLine($"({point2.X} {point2.Y})");
+class Point
+{
+   public float X { get; }
+   public float Y { get; }
+
+   public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public Point() : this(0,0)  {}
+}
