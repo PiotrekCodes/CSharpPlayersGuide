b4fbb0155db85f7325d3700a5193703b8a40d9dc
Piotr Krawczyk <sztymperjan@gmail.com>
2023-06-03 12:22:52 +0100

    feat: Added solution for The Locked Door exercise

---
+++ The Card/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Objectives:
+•  Define enumerations for card colors and card ranks.
+• Define a Card class to represent a card with a color and a rank, as described above.
+• Add properties or methods that tell you if a card is a number or symbol card (the equivalent of a 
+face card).
+• Create a main method that will create a card instance for the whole deck (every color with every 
+rank) and display each (for example, “The Red Ampersand” and “The Blue Seven”).
+ */
+
+Color[] colors = new Color[] { Color.red, Color.green, Color.blue, Color.yellow };
+Rank[] ranks = new Rank[] { Rank.One, Rank.Two, Rank.Three, Rank.Four, Rank.Five, Rank.Six, Rank.Seven, Rank.Eight, Rank.Nine, Rank.Ten, Rank.DollarSign, Rank.Percent, Rank.Caret, Rank.Ampersand };
+
+
+foreach (Color color in colors)
+{
+    foreach (Rank rank in ranks)
+    {
+        Card card = new Card(rank, color);
+        Console.WriteLine($"The {card.Rank} of {card.Color}");
+    }
+}
+
+public class Card
+{
+    public Rank Rank { get; }
+    public Color Color { get; }
+    
+    public Card(Rank rank, Color color)
+    {
+        Rank = rank;
+        Color = color;
+    }
+    
+    public bool IsSymbolCard => Rank is Rank.Ampersand or Rank.Caret or Rank.DollarSign or Rank.Percent;
+    public bool IsNumberCard => !IsSymbolCard;
+    
+    
+}
+
+
+
+public enum Color
+{
+    red, green, blue, yellow
+}
+
+public enum Rank
+{
+    One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, DollarSign, Percent, Caret, Ampersand
+}
---
+++ The Locked Door/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+• An open door can always be closed. 
+• A closed (but not locked) door can always be opened.
+• A closed door can always be locked.
+• A locked door can be unlocked, but a numeric passcode is needed, and the door will only unlock if 
+the code supplied matches the door’s current passcode.
+• When a door is created, it must be given an initial passcode.
+• Additionally, you should be able to change the passcode by supplying the current code and a new 
+one. The passcode should only change if the correct, current code is given.
+
+Objectives:
+• Define a Door class that can keep track of whether it is locked, open, or closed.
+• Make it so you can perform the four transitions defined above with methods. 
+• Build a constructor that requires the starting numeric passcode.
+• Build a method that will allow you to change the passcode for an existing door by supplying the 
+current passcode and new passcode. Only change the passcode if the current passcode is correct.
+• Make your main method ask the user for a starting passcode, then create a new Door instance. Allow 
+the user to attempt the four transitions described above (open, close, lock, unlock) and change the 
+code by typing in text commands.
+*/
+
+
+// Ask for initial passcode
+Console.WriteLine("Please set an initial passcode for the doors: ");
+int initialPasscode = Convert.ToInt32(Console.ReadLine());
+
+// Create a new door instance using initial passcode
+Door door = new Door(initialPasscode);
+
+while (true)
+{
+    // Ask user for commands
+    Console.WriteLine($"The doors are now {door.State}. What would you like to do? (open, close, lock, unlock, change passcode)");
+    string? command = Console.ReadLine();
+
+    switch (command)
+    {
+        case "open":
+            door.Open();
+            break;
+        case "close":
+            door.Close();
+            break;
+        case "lock":
+            door.Lock();
+            break;
+        case "unlock":
+            Console.WriteLine("Please enter the passcode: ");
+            int passcode = Convert.ToInt32(Console.ReadLine());
+            door.Unlock(passcode);
+            break;
+        case "change passcode":
+            Console.WriteLine("Please enter the current passcode: ");
+            int currentPasscode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the new passcode: ");
+            int newPasscode = Convert.ToInt32(Console.ReadLine());
+            door.ChangePasscode(currentPasscode, newPasscode);
+            break;
+    }
+}
+
+
+public class Door
+{
+    private int _passcode;
+    public DoorState State { get; private set; }
+    
+    public Door(int passcode)
+    {
+        _passcode = passcode;
+        State = DoorState.closed;
+    }
+    public void Lock()
+    { if(State == DoorState.closed) State = DoorState.locked; }
+    public void Unlock(int passcode)
+    { if(State == DoorState.locked && passcode == _passcode) State = DoorState.closed; }
+    public void Close()
+    { if(State == DoorState.open) State = DoorState.closed; }
+    public void Open()
+    { if(State == DoorState.closed) State = DoorState.open; }
+    public void ChangePasscode(int oldPasscode, int newPasscode)
+    {if(oldPasscode == _passcode) _passcode = newPasscode;} 
+}
+
+public enum DoorState  {locked, closed, open};
